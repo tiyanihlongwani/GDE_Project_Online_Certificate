4e6b00af4eed9fa6bee1abdd40071e2cd59bfd1a
thlon <thlongwani@hotmail.com>
2021-04-15 13:17:13 +0200

    Initial commit

---
+++ GDEProjectOnlineCertificate/Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GDEProjectOnlineCertificate
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (chkIsActive.Checked)
+                btnCreateAccount.Enabled = true;
+            else
+                btnCreateAccount.Enabled = false;
+        }
+        protected void btnCreateAccount_Click(object sender, EventArgs e)
+        {
+
+            Response.Redirect("/Pages/Account/LoginPage.aspx");
+        }
+
+        protected void btnHome_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Home.aspx");
+        }
+    }
+}
